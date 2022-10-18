6a542e43104248c999e8b0f6a6f0666ac110872f
Kaitlyn Wong <kaitlynwong@Kaitlyns-MacBook-Pro.local>
2022-10-18 09:33:40 -0400

    a6

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+// import { Twitter } from 'react-bootstrap-icons';
 
 const NavigationSidebar = (
     {
@@ -7,36 +8,35 @@ const NavigationSidebar = (
 ) => {
     return (
         <div className="list-group">
-            <a className="list-group-item">Tuiter</a>
-            <a className={`list-group-item
+            <a href = {'../../../public/tuiter/home.html'} className={`list-group-item
                     ${active === 'home'?'active':''}`}>
                 Home
             </a>
-            <a className={`list-group-item
+            <a href = {'../../../public/tuiter/explore/index.html'} className={`list-group-item
                     ${active === 'explore'?'active':''}`}>
                 Explore
             </a>
-            <a className={`list-group-item
+            <a href = {'../../../public/tuiter/notifications.html'} className={`list-group-item
                     ${active === 'notifications'?'active':''}`}>
                 Notifications
             </a>
-            <a className={`list-group-item
+            <a href = {'../../../public/tuiter/messages.html'} className={`list-group-item
                     ${active === 'messages'?'active':''}`}>
                 Messages
             </a>
-            <a className={`list-group-item
+            <a href = {'../../../public/tuiter/bookmarks.html'} className={`list-group-item
                     ${active === 'bookmarks'?'active':''}`}>
                 Bookmarks
             </a>
-            <a className={`list-group-item
+            <a href = {'../../../public/tuiter/lists.html'} className={`list-group-item
                     ${active === 'lists'?'active':''}`}>
                 Lists
             </a>
-            <a className={`list-group-item
+            <a href = {'../../../public/tuiter/profile.html'} className={`list-group-item
                     ${active === 'profile'?'active':''}`}>
                 Profile
             </a>
-            <a className={`list-group-item
+            <a href = {'../../../public/tuiter/more.html'} className={`list-group-item
                     ${active === 'more'?'active':''}`}>
                 More
             </a>
