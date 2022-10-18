26512808d20d26cb0a03b0add8e13f26bee42eab
Kaitlyn Wong <kaitlynwong@Kaitlyns-MacBook-Pro.local>
2022-10-18 00:49:57 -0400

    a6

---
+++ src/labs/index.js
@@ -0,0 +1,11 @@
+import Assignment6 from "../labs/a6";
+import Nav from "../nav";
+function Labs() {
+    return (
+        <div>
+            <Nav/>
+            <Assignment6/>
+        </div>
+    );
+}
+export default Labs;
